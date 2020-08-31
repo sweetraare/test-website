e35253c3ba492d1f15a61f4781dcb18680aa3517
Alex Ulloa <ajua_1696@hotmail.com>
2020-08-30 23:33:13 -0500

    Add animations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   controller = new ScrollMagic.Controller();
@@ -7,7 +8,7 @@ function animateSlides() {
   const slides = document.querySelectorAll(".slide");
   const nav = document.querySelector(".nav-header");
 
-  slides.forEach((slide) => {
+  slides.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -23,6 +24,37 @@ function animateSlides() {
     slidesTimeline.fromTo(img, { scale: 2 }, { scale: 1 }, "-=0.9");
     slidesTimeline.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     slidesTimeline.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false
+    })
+      .addIndicators()
+      .setTween(slidesTimeline)
+      .addTo(controller);
+
+    // page animation
+
+    const pageTimeline = gsap.timeline();
+
+    let nextSlide = index === slides.length - 1 ? "last" : slides[index + 1];
+
+    pageTimeline.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+
+    pageTimeline.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+
+    pageTimeline.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0
+    })
+      .setPin(slide, { pushFollowers: false })
+      .addIndicators()
+      .setTween(pageTimeline)
+      .addTo(controller);
   });
 }
 
